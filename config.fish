569155059d5bf9311a863f183a4a93b0b2c50ac5
Necdet Guner <necdetguner@outlook.com>
2024-04-23 20:57:31 +0300

    add alias part to config.fish

--- config.fish
+++ config.fish
@@ -17,3 +17,12 @@ set -g __fish_git_prompt_color_dirtystate yellow
 set -g __fish_git_prompt_color_cleanstate green --bold
 set -g __fish_git_prompt_color_invalidstate red
 set -g __fish_git_prompt_color_branch cyan --dim --italics
+
+# alias
+alias 'gsync' 'git fetch upstream; git checkout master; git merge upstream/master; git push origin master'
+
+alias 'dnsflush' 'sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
+alias 'dnsget' 'networksetup -getdnsservers Wi-Fi'
+alias 'dnssetlocal' 'networksetup -setdnsservers Wi-Fi 127.0.0.1'
+alias 'dnssetgoogle' 'networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4'
+alias 'dnssetempty' 'networksetup -setdnsservers Wi-Fi empty'
